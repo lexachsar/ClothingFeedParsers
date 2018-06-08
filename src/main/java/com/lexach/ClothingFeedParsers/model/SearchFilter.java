a93bff345cff4836682e3ca8384cc484b3f9af4d
Alex Chernevskiy <lexachsar@gmail.com>
2018-06-08 09:00:31 +0400

    Model update.

--- src/main/java/com/lexach/ClothingFeedParsers/model/SearchFilter.java
+++ src/main/java/com/lexach/ClothingFeedParsers/model/SearchFilter.java
@@ -1,4 +1,4 @@
-package com.lexach.ClothingFeed.model;
+package com.lexach.ClothingFeedParsers.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.annotation.CreatedDate;
